815a2540485f727d0c3b0bb72c5b8097d71567a0
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-08 21:06:32 +0300

    using yargs with required file running with EX: node app.js read --title=dick

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const yargs = require('yargs')
 const notes = require('./notes.js')
 
 
+//argv is the option used running node 
+//Ex: node app.js add --title=secrets --body="This is my secret"
 const argv = yargs.argv
 var command = process.argv[2]
 console.log('Command:', command)
-console.log('Process:', process.argv)
 console.log('Yargs:', argv)
 
 if (command === 'add') {
-    // console.log('Adding new note')
     notes.addNote(argv.title, argv.body)
 } else if (command === 'list') {
-    console.log('Listing all notes')
+    notes.getAll()
 } else if (command === 'read') {
-    console.log('Reading note')
+    notes.getNote(argv.title)
 } else if (command === 'remove') {
-    console.log('Removing note')
+    notes.removeNote(argv.title)
 } else {
     console.log('Command not recognized')
 }
--- notes.js
+++ notes.js
@@ -4,8 +4,25 @@ var addNote = (title, body) => {
     console.log('Adding note', title, body)
 }
 
+var getAll = () => {
+    console.log('Getting all notes')
+}
+
+var getNote = (title) => {
+    console.log('Getting note', title)
+}
+
+var removeNote = (title) => {
+    console.log('Removing note', title)
+}
+
 module.exports = {
-    addNote //in es6 identical to addNote : addNote (if obj key same name as obj property)
+    //in es6 identical to addNote : addNote (if obj key same name as obj property)
+    addNote,
+    //above is es6 syntax explained, below is previos es6 to remember
+    getAll: getAll,
+    getNote: getNote,
+    removeNote: removeNote,
 }
 
 
