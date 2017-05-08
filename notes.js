ed2460280c90d1b0d4b3fce47849fee5aef6cb3a
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-08 20:53:00 +0300

    add yarn, running app with node app.js add --title=secrets --body="This is my secret" will output title and body

--- notes.js
+++ notes.js
@@ -1,9 +1,18 @@
 console.log('Starting notes.js')
 
+var addNote = (title, body) => {
+    console.log('Adding note', title, body)
+}
+
+module.exports = {
+    addNote //in es6 identical to addNote : addNote (if obj key same name as obj property)
+}
+
+
 // console.log(module) //output ... exports: {}, ...
 // module.exports.age = 25 //add a property to the exports object
 
-module.exports.addNote = () => {
-    console.log('addNote')
-    return 'New note'
-}
+// module.exports.addNote = () => {
+//     console.log('addNote')
+//     return 'New note'
+// }
