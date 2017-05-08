bd6c80317563755b0f36c1d12b7d3be45c426a78
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-08 19:50:23 +0300

    adding params to node on run EX: 'node app add'

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const fs = require('fs')
 const _ = require('lodash')
 
 const notes = require('./notes.js')
+
+var command = process.argv[2]
+
+if (command === 'add') {
+    console.log('Adding new note')
+} else if (command === 'list') {
+    console.log('Listing all notes')
+} else {
+    console.log('Command not recognized')
+}
